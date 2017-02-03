bb22f85521e86de30d865623c18405766437b29c
headhuntar <headhuntar@gmail.com>
2017-02-03 14:20:25 -0800

    connect denied due to firewall

--- src/Email_Logger.java
+++ src/Email_Logger.java
@@ -1,4 +1,16 @@
+import org.apache.log4j.Logger;
 
 public class Email_Logger {
-	testing
+	
+	static Logger log = Logger.getLogger(Email_Logger.class);
+	
+	public static void main (String args[]){
+		System.out.println("testing");
+		try{
+			log.error("email this");
+		}catch(Exception ex){
+			System.out.println("exception caught: " + ex.getMessage());
+		}
+		System.out.println("Ending");
+	}
 }
